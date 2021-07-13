f9d493d8fb7e247a21bde0f21d29ffc1bddaf2ca
devtechsolution <srivastvadevzone@gamil.com>
2021-07-13 23:01:42 +0530

    Implemented circuit breaker

--- order-service/src/main/java/org/as/devtechsolution/order/controller/OrderController.java
+++ order-service/src/main/java/org/as/devtechsolution/order/controller/OrderController.java
@@ -1,12 +1,15 @@
 package org.as.devtechsolution.order.controller;
 
 import java.util.UUID;
+import java.util.function.Supplier;
 
 import org.as.devtechsolution.order.client.InventoryClient;
 import org.as.devtechsolution.order.dto.OrderDto;
 import org.as.devtechsolution.order.entity.Order;
 import org.as.devtechsolution.order.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreaker;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,29 +17,32 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/api/order")
-//@RequiredArgsConstructor
+@RequiredArgsConstructor
 @Slf4j
 public class OrderController {
 
 	private final OrderRepository orderRepository;
 	private final InventoryClient inventoryClient;
-	
-	
-	
+	private final Resilience4JCircuitBreakerFactory circuitBreakerFactory;
 
 	@PostMapping
 	public ResponseEntity<?> placeOrder(@RequestBody OrderDto orderDto) {
-		boolean allProductInStock = orderDto.getOrderLineItemsList().stream()
+
+		Resilience4JCircuitBreaker circuitBreaker = circuitBreakerFactory.create("inventory");
+		Supplier<Boolean> booleanSupplier = () -> orderDto.getOrderLineItemsList().stream()
 				.allMatch(orderItem -> inventoryClient.checkStock(orderItem.getSkuCode()));
-		
+
+		boolean allProductInStock = circuitBreaker.run(booleanSupplier, throwable-> handleErrorCase());
+
 		if (allProductInStock) {
-			Order order = Order.builder().orderNumber(UUID.randomUUID().toString()).orderLineItems(orderDto.getOrderLineItemsList())
-					.build();
+			Order order = Order.builder().orderNumber(UUID.randomUUID().toString())
+					.orderLineItems(orderDto.getOrderLineItemsList()).build();
 			return new ResponseEntity<>(orderRepository.save(order), HttpStatus.CREATED);
 		} else {
 			return new ResponseEntity<>("Order failed, one of the products in the order is not in stock",
@@ -48,9 +54,4 @@ public class OrderController {
 		return false;
 	}
 
-	public OrderController(OrderRepository orderRepository, InventoryClient inventoryClient) {
-		super();
-		this.orderRepository = orderRepository;
-		this.inventoryClient = inventoryClient;
-	}
 }
